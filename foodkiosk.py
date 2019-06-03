832843bfe1bfaf1e510357d360097e55810f3846
KronosEmporium <kronosemporium12@gmail.com>
2019-06-03 01:25:40 -0700

    Added test cases to foodkiosk.py

--- foodkiosk.py
+++ foodkiosk.py
@@ -79,12 +79,14 @@ class OrderItem(MenuItem):
     self.__orderItemModifications = mods
 
 class Order:
-  def __init__(self, id, details):
+  def __init__(self, id, details, dis, distype):
     self.__orderID = id
     self.__orderItems = []
     self.__ordertime = 0
     self.__orderDetails = details
     self.__orderTotal = 0
+    self.__discount = dis
+    self.__discountType = distype
 
     # By default, the order time is set when the Order is instantiated. Use self.setOrderTime to override.
     self.setOrderTime(datetime.datetime.now())
@@ -153,6 +155,27 @@ class Order:
     _orderItemPrices = [x.getMenuItemPrice() for x in self.getOrderItems()]
     self.__orderTotal = sum(_orderItemPrices)
 
+    if (self.__discount != None):
+      self.applyDiscount()
+
+  def getOrderDiscount(self):
+    return self.__discount
+
+  def setOrderDiscount(self, dis):
+    self.__discount = dis
+
+  def getOrderDiscountType(self):
+    return self.__discountType
+
+  def setOrderDiscountType(self, distype):
+    self.__discountType = distype
+
+  def applyDiscount(self):
+    if (self.__discountType == "flat"):
+      self.__orderTotal -= self.__discount
+    elif (self.__discountType == "percent"):
+      self.__orderTotal -= (self.__orderTotal * (self.__discount / 100))
+
 class Receipt:
   def __init__(self, id, cname, order, msg):
     self.__receiptID = id
@@ -192,26 +215,89 @@ class Receipt:
   def setReceiptMessage(self, msg):
     self.__receiptMessage = msg
 
-
-# Tests
-item1 = MenuItem(0, 10, "Regular Burger", "A really tasty burger.")
-item2 = MenuItem(1, 11, "Irregular Burger", "An even tastier burger.")
-
-oitem1 = OrderItem(0, item1.getMenuItemID(), item1.getMenuItemPrice(), item1.getMenuItemName(), item1.getMenuItemDescription(), "no cheese")
-oitem2 = OrderItem(2, item2.getMenuItemID(), item2.getMenuItemPrice(), item2.getMenuItemName(), item2.getMenuItemDescription(), "extra cheese")
-
-order1 = Order(0, "Order 1")
-order1.addOrderItem(oitem1)
-order1.addOrderItem(oitem2)
-
-# Make a receipt with ID 0, cashier "Doug", order = order1, message "Have a great day!". Prints mock receipt.
-receipt1 = Receipt(0, "Doug", order1, "Have a great day!")
-print("""Thank you for dining with us.
+  def printReceipt(self):
+    print("""----------------------------
+Thank you for dining with us.
 Receipt No.: %d
 Cashier: %s
 
 Items:
 %s
 
+Discount: %d (%s)
 Order Total: $%.2f
-%s""" % (receipt1.getReceiptID(), receipt1.getCashierName(), "\n".join([x.getMenuItemName() + (" $%.2f" % x.getMenuItemPrice()) for x in receipt1.getOrder().getOrderItems()]), receipt1.getOrder().getOrderTotal(), receipt1.getReceiptMessage()))
+%s""" % (self.getReceiptID(), self.getCashierName(), 
+  "\n".join([x.getMenuItemName() + (" $%.2f" % x.getMenuItemPrice()) for x in self.getOrder().getOrderItems()]), 
+  self.getOrder().getOrderDiscount(), self.getOrder().getOrderDiscountType(),
+  self.getOrder().getOrderTotal(), self.getReceiptMessage()) + "\n")
+
+
+# Tests
+
+# Test Menu Items
+item1 = MenuItem(0, 10, "Regular Burger", desc=None)
+item2 = MenuItem(1, 11, "Irregular Burger", desc=None)
+item3 = MenuItem(2, 6, "Fries", desc=None)
+item4 = MenuItem(3, 6, "Tater Tots", desc=None)
+
+#1 Make a receipt with ID 0, cashier "Doug", order = order1, message "Have a great day!". Prints mock receipt.
+oitem1 = OrderItem(0, item1.getMenuItemID(), item1.getMenuItemPrice(), item1.getMenuItemName(), item1.getMenuItemDescription(), "no cheese")
+oitem2 = OrderItem(2, item2.getMenuItemID(), item2.getMenuItemPrice(), item2.getMenuItemName(), item2.getMenuItemDescription(), "extra cheese")
+order = Order(0, "Order 1", 0, None)
+order.addOrderItem(oitem1)
+order.addOrderItem(oitem2)
+receipt = Receipt(1, "Doug", order, "Have a great day!")
+receipt.printReceipt()
+del order, receipt
+
+#2 Make an Order where Customer adds two orders of fries then removes an order of fries.
+oitem1 = OrderItem(0, item3.getMenuItemID(), item3.getMenuItemPrice(), item3.getMenuItemName(), item3.getMenuItemDescription(), mods=None) # Fries with no modifiers
+order = Order(0, "Order 1", 0, None)
+order.addOrderItem(oitem1)
+order.addOrderItem(oitem1)
+order.removeOrderItem(oitem1)
+receipt = Receipt(2, "Albert", order, "Have a great day!")
+receipt.printReceipt()
+
+#3 Change receipt message
+receipt.setReceiptMessage("Please come again!")
+receipt.setReceiptID(3)
+receipt.printReceipt()
+
+#4 Remove all items from Order and add two Regular Burgers with no mods
+oitem1 = OrderItem(0, item1.getMenuItemID(), item1.getMenuItemPrice(), item1.getMenuItemName(), item1.getMenuItemDescription(), mods=None)
+order.clearOrderItems();
+order.addOrderItem(oitem1);
+order.addOrderItem(oitem1);
+receipt.printReceipt();
+
+#5 Albert the cashier clocks out, change Cashier name on Receipt to Peter
+receipt.setCashierName("Peter")
+receipt.setReceiptID(4)
+receipt.printReceipt()
+
+#6 Apply a $10 discount to the order
+order.setOrderDiscount(10)
+order.setOrderDiscountType("flat")
+order.applyDiscount()
+receipt.setReceiptID(5)
+receipt.printReceipt()
+
+#7 Change the discount to a percentage
+order.setOrderDiscountType("percent")
+receipt.setReceiptID(6)
+receipt.printReceipt()
+del order, receipt
+
+#8 Create new Menu item and change its price after creating
+item5 = MenuItem(4, 15, "Sushi Burger", desc=None)
+item5.setMenuItemPrice(14)
+
+#9 Create Order item of new item with extra wasabi
+oitem1 = OrderItem(0, item5.getMenuItemID(), item5.getMenuItemPrice(), item5.getMenuItemName(), item5.getMenuItemDescription(), mods="extra wasabi")
+
+#10 Add to a new Order and apply a 5% discount.
+order = Order(0, "Order 1", 5, "percent")
+order.addOrderItem(oitem1)
+receipt = Receipt(7, "Peter", order, "Please come again!")
+receipt.printReceipt()
